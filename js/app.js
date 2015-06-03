33d1c62a18463d73a575853db8b393753213fb4b
unknown <dan.dh.lee@gmail.com>
2015-06-02 19:51:14 -0700

    Add loading route animation

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-angular.module('myApp', ['ngRoute'])
+angular.module('myApp', ['ngRoute', 'ngAnimate'])
 	.config(['$routeProvider', function($routeProvider) {
 		$routeProvider.when('/', {
 			templateUrl: '../views/description.html'
@@ -11,5 +11,18 @@ angular.module('myApp', ['ngRoute'])
 			template: '<main class="error"><p>Error - Page Not Found</p></main>'
 		})
 		.otherwise('/error');
-	}]);
+	}])
+	.run(function($rootScope, $location, $timeout) {
+	    $rootScope.$on('$routeChangeError', function() {
+	        $location.path("/error");
+	    });
+	    $rootScope.$on('$routeChangeStart', function() {
+	        $rootScope.isLoading = true;
+	    });
+	    $rootScope.$on('$routeChangeSuccess', function() {
+	      $timeout(function() {
+	        $rootScope.isLoading = false;
+	      }, 1000);
+	    });
+	});
 	
